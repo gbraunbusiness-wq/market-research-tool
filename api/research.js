b35493aa51f1f0c26d9f69a4a78731745d744998
Gavin Braun <gbraunbusiness@gmail.com>
2026-06-17 19:41:33 -0500

    fix api handler

--- api/research.js
+++ api/research.js
@@ -1,4 +1,6 @@
-export default async function handler(req, res) {
+const fetch = require('node-fetch');
+
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
